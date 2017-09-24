da089105e40be698a93e08c91a88dd2fadb17fb3
James <root@localhost.localdomain>
2017-09-24 12:47:10 +0000

    Ensure text files are displayed with correct encoding

--- main.js
+++ main.js
@@ -13,17 +13,15 @@ const FILE_PATH = ((prospectivePath) => {
 		((process.argv.length >= 3 && typeof process.argv[2] === "string")
 			? process.argv[2] : "")
 	),
-	DATA_PATH = "./data/",
+	DATA_PATH = PATH.join(__dirname, "data/"),
 	ERROR_404_TEXT = "NOT FOUND",
+	READ_FILE_ENCODING = "utf-8",
+	READ_FILE_OPTIONS = { encoding: READ_FILE_ENCODING },
 	EM_DASH = "—",
 	PORT = 443;
 
-let privateKey = FS.readFileSync(PATH.join(DATA_PATH, "key.pem"), {
-		encoding: "utf-8"
-	}),
-	certificate = FS.readFileSync(PATH.join(DATA_PATH, "cert.pem"), {
-		encoding: "utf-8"
-	}),
+let privateKey = FS.readFileSync(PATH.join(DATA_PATH, "key.pem"), READ_FILE_OPTIONS),
+	certificate = FS.readFileSync(PATH.join(DATA_PATH, "cert.pem"), READ_FILE_OPTIONS),
 	credentials = {
 		key: privateKey,
 		cert: certificate
@@ -54,11 +52,9 @@ let privateKey = FS.readFileSync(PATH.join(DATA_PATH, "key.pem"), {
 
 		try {
 			fileDescriptor = FS.openSync(filePath, "r");
-			fileContents = FS.readFileSync(fileDescriptor, {
-				encoding: "utf-8"
-			});
+			fileContents = FS.readFileSync(fileDescriptor, READ_FILE_OPTIONS);
 		} catch(e) {} finally {
-			if (typeof fileDescriptor === "number") {
+			if (fileDescriptor != null) {
 				FS.closeSync(fileDescriptor);
 			}
 		}
@@ -69,6 +65,10 @@ let privateKey = FS.readFileSync(PATH.join(DATA_PATH, "key.pem"), {
 			logText += ["", EM_DASH, ERROR_404_TEXT].join(" ");
 		} else {
 			if (typeof fileType === "string" && fileType.length) {
+				if (fileType.indexOf("text") === 0 || fileType.indexOf("application") === 0) {
+					fileType += "; charset=" + READ_FILE_ENCODING;
+				}
+
 				res.setHeader("Content-Type", fileType);
 			}
 
